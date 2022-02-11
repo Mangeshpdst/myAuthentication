40d5fee8323d3f6095bfd889bcb435ca4fc6f6e7
Indiancommodities <mangeshpdst@gmail.com>
2022-02-11 16:51:41 +0530

    basic dynamic phone entery

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { Component, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      number: '', 
+      otp: ''
+    }
+  }
+  sendOTP = () => {
+    this.setState({
+      otp: this.state.number
+    })
+  }
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.textInput} 
+      placeholder="Enter Number"
+      maxLength={10}
+      
+      onChangeText={number => this.setState({number}) } 
+    
+      value={this.state.number}
+      keyboardType="numeric"/>
+
+
+
+      <Button title='Get Otp' style={styles.btn} 
+      onPress={this.sendOTP}
+      />
+      <Text value={this.state.otp}>{this.state.otp}</Text>
     </View>
   );
-}
+}}
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +44,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    width: "80%",
+    backgroundColor: "#e6e9ed",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+    textAlign: "center",
+    fontSize: 20
+  },
+  
 });
